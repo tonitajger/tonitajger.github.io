74a5ea4e3d8db7b34734d0e8ab8218c3ff634ec4
Tony Liu <tony.liu.se@gmail.com>
2018-05-17 19:52:26 +0200

    implementing scroll on navlinks

--- js/index.js
+++ js/index.js
@@ -5,4 +5,25 @@ $(document).init(function() {
       $("#hidden-bubble").css("filter","opacity(0%)");
     }, 3000);
   })
+  $("#top-link").click(function() {
+    $('html, body').animate({
+    scrollTop: $("#top").offset().top
+    }, 300);
+  })
+  $("#project-link").click(function() {
+    $('html, body').animate({
+    scrollTop: $("#projects").offset().top
+    }, 300);
+  })
+  $("#about-link").click(function() {
+    $('html, body').animate({
+    scrollTop: $("#about").offset().top
+    }, 300);
+  })
+  $("#resume-link").click(function() {
+    $('html, body').animate({
+    scrollTop: $("#resume").offset().top
+    }, 300);
+  })
+  /*elmnt.scrollIntoView();*/
 })
