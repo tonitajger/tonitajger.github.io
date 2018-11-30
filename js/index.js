613e78172f0653baba7f8833ef620a8c1081255c
tonitajger <tony.liu.se@gmail.com>
2018-11-30 22:23:49 +0100

    Website rmk

--- js/index.js
+++ js/index.js
@@ -1,29 +1,19 @@
-$(document).init(function() {
-  $(".img-circle").click(function() {
-    $("#hidden-bubble").css("filter","opacity(100%)");
-    setTimeout(function() {
-      $("#hidden-bubble").css("filter","opacity(0%)");
-    }, 3000);
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    //options here
+    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+    autoScrolling: true,
+    scrollHorizontally: true,
+    
+    anchors: ['page1', 'page2', 'page3', 'page4'],
+    
+  });
+
+  $('.arrow-down').click(function(){
+    fullpage_api.moveSectionDown();
   })
-  $("#top-link").click(function() {
-    $('html, body').animate({
-    scrollTop: $("#top").offset().top
-    }, 300);
-  })
-  $("#project-link").click(function() {
-    $('html, body').animate({
-    scrollTop: $("#projects").offset().top
-    }, 300);
-  })
-  $("#about-link").click(function() {
-    $('html, body').animate({
-    scrollTop: $("#about").offset().top
-    }, 300);
-  })
-  $("#resume-link").click(function() {
-    $('html, body').animate({
-    scrollTop: $("#resume").offset().top
-    }, 300);
-  })
-  /*elmnt.scrollIntoView();*/
-})
+
+
+});
+
+
